260ea71ff16078e274255aea419838e2974f5c9f
Pessanha <47800089+vpessanha@users.noreply.github.com>
2021-02-17 18:16:20 -0300

    Update run-as-admin.ps1

--- run-as-admin.ps1
+++ run-as-admin.ps1
@@ -1,15 +1,22 @@
-﻿########################## Pegar o controle da chave de registro ##########################
+$Host.UI.RawUI.WindowTitle = "Run As Admin"
+set-executionpolicy remotesigned -force
+
+Write-Host "------------------------------------------------------------"
+Write-Host "     (c) 2021 - https://gitlab.com/vpess/Run-as-Admin"
+Write-Host "------------------------------------------------------------`n"
+
+Write-Host "Criando ambiente admin. Aguarde..."
+Start-Sleep -s 1
 
 $nomeUsuario = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $usuario = new-object System.Security.Principal.NTAccount($nomeUsuario)
-New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR | Out-Null
 Set-location HKCR:\AppID\
 (get-acl '{CDCBCFCA-3CDC-436f-A4E2-0E02075250C2}').setowner($usuario)
+Start-Sleep -s 1
 
-########################## Atribuir permissão ao admin ##########################
 
-$acl = Get-Acl "HKCR:\AppID\{CDCBCFCA-3CDC-436f-A4E2-0E02075250C2}"
-#$usuario = new-object System.Security.Principal.NTAccount($nomeUsuario)       
+$acl = Get-Acl "HKCR:\AppID\{CDCBCFCA-3CDC-436f-A4E2-0E02075250C2}"       
 $access = [System.Security.AccessControl.RegistryRights]"FullControl"
 $inheritance = [System.Security.AccessControl.InheritanceFlags]"ContainerInherit,ObjectInherit"
 $propagation = [System.Security.AccessControl.PropagationFlags]"None"
@@ -18,8 +25,78 @@ $rule = New-Object System.Security.AccessControl.RegistryAccessRule($usuario,$ac
 $acl.AddAccessRule($rule)
 $acl | Set-Acl
 
-############# Renomear o valor de Runas ##########################
-Rename-ItemProperty -Path 'HKCR:\AppID\{CDCBCFCA-3CDC-436f-A4E2-0E02075250C2}' -Name Runas -NewName Runas_old
+Rename-ItemProperty -Path 'HKCR:\AppID\{CDCBCFCA-3CDC-436f-A4E2-0E02075250C2}' -Name Runas -NewName Runas_
+Start-Sleep -s 1
 
-############# Abrir explorer.exe como admin ##########################
-start-process explorer.exe /separate -verb RunAs
+function selec {
+
+    param (
+    [string]$Titulo = 'Menu'
+    )
+    
+    Write-Host "`n============================ Run As Admin ===========================`n" -ForegroundColor Blue
+    
+    Write-Host "	[1] Windows Explorer"
+    Write-Host "	[2] Desinstalar um programa"
+    Write-Host "	[3] Gerenciamento do Sistema (permissões, drivers, etc)"
+    Write-Host "	[4] Painel de Controle"
+    Write-Host "	[5] Propriedades do sistema (criação de variável)"
+    Write-Host "	[q] para fechar o script" -ForegroundColor Red
+    
+    Write-Host "`n=====================================================================" -ForegroundColor Blue
+    
+     $selection = Read-Host "`nSelecione uma das opções acima"
+     switch ($selection)
+     {
+    
+         '1' {explorer.exe /separate -verb RunAs
+            Start-Sleep -s 1
+            return selec}
+    
+         '2' {start-process appwiz.cpl -verb RunAs
+            Start-Sleep -s 1
+            return selec}
+    
+         '3' {start-process compmgmt.msc -verb RunAs
+            Start-Sleep -s 1
+            return selec}
+
+         '4' {start-process control -verb RunAs
+            Start-Sleep -s 1
+            return selec}
+
+         '5' {start-process sysdm.cpl -verb RunAs
+            Start-Sleep -s 1
+            return selec}
+    
+         'q'{
+            Write-Host "`nSaindo..." -ForegroundColor Red
+            Start-sleep -s 2
+            return
+         }
+    
+         default {
+    
+            if ($selection -ige 6 -or $selection -ne 'q'){
+                 Write-Host "`n>>> Selecione apenas opções que estejam no menu!" -ForegroundColor Red
+                 Start-Sleep -s 2
+                 return selec
+                 }
+            }
+     }
+    
+}
+    
+    selec
+
+
+
+
+
+<#Referências:
+
+- https://www.alkanesolutions.co.uk/2016/06/29/set-registry-key-permissions-powershell/
+- https://stackoverflow.com/questions/12044432/how-do-i-take-ownership-of-a-registry-key-via-powershell
+- https://stackoverflow.com/questions/46586382/hide-powershell-output
+
+#>
