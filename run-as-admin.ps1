ec74d4e71a9b9e77c68a053867d0bff03582bc09
Pessanha <47800089+vpessanha@users.noreply.github.com>
2021-02-24 17:57:37 -0300

    Update run-as-admin.ps1

--- run-as-admin.ps1
+++ run-as-admin.ps1
@@ -1,8 +1,24 @@
 $Host.UI.RawUI.WindowTitle = "Run As Admin"
 
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) { #credits to https://github.com/Sycnex/Windows10Debloater/blob/master/Windows10Debloater.ps1
+<#If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) { #credits to https://github.com/Sycnex/Windows10Debloater/blob/master/Windows10Debloater.ps1
    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
    Exit
+}
+#>
+
+function Test-Admin {
+   param([switch]$Elevated)
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
 }
 
 Write-Host "------------------------------------------------------------"
@@ -11,6 +27,8 @@ Write-Host "------------------------------------------------------------`n"
 
 Write-Host "Criando ambiente admin. Aguarde..."
 Start-Sleep -s 1
+
+function Get-Reg {
 
 #$nomeUsuario = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $nomeUsuario = whoami
@@ -32,6 +50,9 @@ $acl | Set-Acl
 
 Rename-ItemProperty -Path 'HKCR:\AppID\{CDCBCFCA-3CDC-436f-A4E2-0E02075250C2}' -Name Runas -NewName Runas_
 Start-Sleep -s 1
+
+}
+Get-Reg
 
 function selec {
 
@@ -95,14 +116,12 @@ function selec {
     selec
 
 
-
-
-
 <#Referências:
 
 - https://www.alkanesolutions.co.uk/2016/06/29/set-registry-key-permissions-powershell/
 - https://stackoverflow.com/questions/12044432/how-do-i-take-ownership-of-a-registry-key-via-powershell
 - https://stackoverflow.com/questions/46586382/hide-powershell-output
+- https://superuser.com/questions/108207/how-to-run-a-powershell-script-as-administrator
 
 
 #>
